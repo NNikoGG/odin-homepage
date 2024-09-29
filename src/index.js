dd028d0ea25d2881ca5ba8ff483552cbb2c26b47
niko-linux <geekgaming222@gmail.com>
2024-09-29 20:56:01 +0530

    Add header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import "./style.css";
-
-let helloWorld = document.createElement("div");
-helloWorld.innerText = "Hello World!";
-helloWorld.classList.add("hello");
-
-document.body.appendChild(helloWorld);
+import './style.css';
+import './img/profile.png';
